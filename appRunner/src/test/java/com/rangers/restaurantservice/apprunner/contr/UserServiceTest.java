efc913485dda575154f9aeb60e4b1d91f32ebcb5
Hanna Krzh <anutkakrz@gmail.com>
2024-07-12 23:29:52 +0200

    Create UserServiceTest

--- appRunner/src/test/java/com/rangers/restaurantservice/apprunner/contr/UserServiceTest.java
+++ appRunner/src/test/java/com/rangers/restaurantservice/apprunner/contr/UserServiceTest.java
@@ -1,9 +1,12 @@
 package com.rangers.restaurantservice.apprunner.contr;
 
+import com.rangers.restaurantservice.dto.UserDto;
 import com.rangers.restaurantservice.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 
@@ -13,6 +16,14 @@ public class UserServiceTest {
 
     @Test
     void test() {
-        System.out.println(userService.getUserByChatId("479322389"));
+        UserDto userDtoExpected = new UserDto();
+        userDtoExpected.setFirstName("Viktor");
+        userDtoExpected.setLastName("Bulatov");
+        userDtoExpected.setEmail("testemail@gmail.com");
+        userDtoExpected.setPhoneNumber("4738858588388");
+        userDtoExpected.setRole("OWNER");
+        userDtoExpected.setChatId("479322389");
+        UserDto userDtoActual = userService.getUserByChatId("479322389");
+        Assertions.assertEquals(userDtoExpected, userDtoActual);
     }
 }
